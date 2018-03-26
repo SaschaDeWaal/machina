63736fb0576d98b8ec62d3fe0e0935a73a4eba93
Ward Bannink <w.r.j.bannink@students.uu.nl>
2018-03-26 14:10:14 +0200

    Derping around with the motors

---
+++ robot/venv/Scripts/ExternalBehaviour.py
@@ -0,0 +1,11 @@
+from DriveMotor import DriveMotor
+
+
+class ExternalBehaviour:
+
+    def __init__(self, motorA, motorB):
+        self._motorA = motorA
+        self._motorB = motorB
+        #_motorA.setDirection(1)
+
+
