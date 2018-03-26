885f0ea42dad607838c64eb424e8c8ee159ee59b
ishdeep-bhandari <ishdeepbhandari@gmail.com>
2018-03-26 14:13:27 +0200

    new

--- robot/venv/Scripts/ExternalBehaviour.py
+++ robot/venv/Scripts/ExternalBehaviour.py
@@ -3,9 +3,9 @@ from DriveMotor import DriveMotor
 
 class ExternalBehaviour:
 
-    def __init__(self, motorA, motorB):
-        self._motorA = motorA
-        self._motorB = motorB
-        #_motorA.setDirection(1)
+    def __init__(self, leftMotor, rightMotor):
+        self._motorA = leftMotor
+        self._motorB = rightMotor
+        _motorA.setDirection(1)
 
 
