631d9aa0d5ca9af2b3b03bd7738045833b1947b0
Ward Bannink <w.r.j.bannink@students.uu.nl>
2018-03-26 14:16:10 +0200

    Renamed the motors to left and right for easier usage, and created a Sensor class that is to be used to store sensor values.

--- robot/venv/Scripts/ExternalBehaviour.py
+++ robot/venv/Scripts/ExternalBehaviour.py
@@ -1,11 +1,12 @@
 from DriveMotor import DriveMotor
+from InternalBehaviour import InternalBehaviour
 
 
 class ExternalBehaviour:
 
-    def __init__(self, motorA, motorB):
-        self._motorA = motorA
-        self._motorB = motorB
+    def __init__(self, motorLeft, motorRight):
+        self._motorLeft = motorLeft
+        self._motorRight = motorRight
         #_motorA.setDirection(1)
 
 
---
+++ robot/venv/Scripts/Sensor.py
@@ -0,0 +1,7 @@
+"""
+
+This file is meant to take care of, and store, the output given by the sensors of the robot.
+This is for easier storage and usage by other files.
+
+"""
+
