207b2219f4f537cc4532c3a2ce63822b51c29d97
Ward Bannink <w.r.j.bannink@students.uu.nl>
2018-03-26 14:44:49 +0200

    Resolved merging issue

--- robot/venv/Scripts/ExternalBehaviour.py
+++ robot/venv/Scripts/ExternalBehaviour.py
@@ -13,20 +13,9 @@ found in InternalBehaviour.
 
 class ExternalBehaviour:
 
-<<<<<<< HEAD
-    def __init__(self, leftMotor, rightMotor):
-        self._motorLeft = leftMotor
-        self._motorRight = rightMotor
-        _motor.setDirection(1)
-=======
     def __init__(self, motorLeft, motorRight):
         self._motorLeft = motorLeft
         self._motorRight = motorRight
         #_motorA.setDirection(1)
-<<<<<<< HEAD
         InternalBehaviour._funFrust = 5 #test
-=======
->>>>>>> bdb8d7be68fdf2f9480d18658935a468cabe5823
->>>>>>> e8d3e842501e70bc7b3e7dad96a9b952bc0ed9d0
-
 
