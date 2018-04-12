8ca34390aadc3545e55f7e65cc20fab5513da3ba
beatnory <info@dreamfoxgames.com>
2018-04-12 12:51:32 +0200

    Added arduinoBridge object: This objecs stores all sensor data from arduino

---
+++ robot/venv/Scripts/SensorBridge.py
@@ -0,0 +1,27 @@
+from threading import Thread
+import serial
+import sys
+import json
+
+class SensorBridge:
+
+    def __init__(self):
+        self.ser = serial.Serial(port='/dev/ttyS0', baudrate = 115200, timeout = 5)
+        self.lastData = json.loads('{}')
+
+        self.listening = True;
+        self.thread = Thread(target=self.arduinoConnection, args=())
+        self.thread.start()
+
+    def arduinoConnection(self):
+        while self.listening:
+            try:
+                data = self.ser.readline()
+                self.lastData = json.loads(data)
+            except:
+                print "Unexpected error:", sys.exc_info()
+                sys.exit()
+
+    def stop(self):
+        self.listening = False
+        self.ser.close();
