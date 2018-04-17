a485f017d09d66f49eadc0e177f45bdec5bdfe1c
beatnory <info@dreamfoxgames.com>
2018-04-17 10:43:39 +0200

    added temp sensors

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,19 +1,27 @@
+#include <OneWire.h> 
+#include <DallasTemperature.h>
 #include <MPU6050_tockn.h>
 #include <Arduino.h>
 #include<Wire.h>
-
-MPU6050 mpu6050(Wire);
-
-int index = 0;
 
 //pins:
 const int pinLight1 = 0;
 const int pinLight2 = 1;
 const int pinLight3 = 2;
+const int pinTempSensor1 = 3;
+const int pinTempSensor2 = 4;
+const int updateTempEvery = 20;
 
-//const int pinTempSensor1 = 3;
-//const int pinTempSensor2 = 4;
+int index = 0;
+int sensorUpdate = 0;
 
+MPU6050 mpu6050(Wire);
+
+OneWire tempSensorWire1(pinTempSensor1);
+OneWire tempSensorWire2(pinTempSensor2);
+
+DallasTemperature tempSensor1(&tempSensorWire1);
+DallasTemperature tempSensor2(&tempSensorWire2);
 
 void setup() {
   Serial.begin(115200);
@@ -21,16 +29,29 @@ void setup() {
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets();
+  tempSensor1.begin();
+  tempSensor2.begin();
 }
 
 void loop() {
   mpu6050.update();
 
+  sensorUpdate ++;
+
+  if(sensorUpdate == updateTempEvery / 2){
+    tempSensor1.requestTemperatures();
+  }
+
+  if(sensorUpdate > updateTempEvery){
+    tempSensor2.requestTemperatures();
+    sensorUpdate = 0;
+  }
+
   index ++;
   String json = CreateJson(index);
   Serial.println(json);
   
-  delay(200);
+  delay(0);
 }
 
 String CreateJson(int messageIndex){
@@ -38,20 +59,26 @@ String CreateJson(int messageIndex){
   String lightValue2 = String(analogRead(pinLight2));
   String lightValue3 = String(analogRead(pinLight3));
 
-  String tempValue1 = "0";//String(analogRead(pinTempSensor1));
-  String tempValue2 = "0";//String(analogRead(pinTempSensor2));
+  String tempValue1 = String(tempSensor1.getTempCByIndex(0));
+  String tempValue2 = String(tempSensor2.getTempCByIndex(0));
+  String tempValue3 = String(mpu6050.getTemp());
   
   return "{" +
     CreateJsonLine("index", String(messageIndex)) + "," +
-    CreateJsonLine("gyroscoop", "[" + String(mpu6050.getGyroAngleX()) + "," + String(mpu6050.getGyroAngleY()) + "," + String(mpu6050.getGyroAngleZ()) + "]") + "," +
-    CreateJsonLine("Accelerometer", "[" + String(mpu6050.getAccX()) + "," + String(mpu6050.getAccY()) + "," + String(mpu6050.getAccZ()) + "]") + "," +
-    CreateJsonLine("temperature", "[2" + tempValue1 + "," + tempValue2 + "]") + "," +
+    CreateJosnArrayLine("gyroscoop", String(mpu6050.getGyroAngleX()) + "," + String(mpu6050.getGyroAngleY()) + "," + String(mpu6050.getGyroAngleZ())) + "," +
+    CreateJosnArrayLine("CreateJosnArrayLine", String(mpu6050.getAccX()) + "," + String(mpu6050.getAccY()) + "," + String(mpu6050.getAccZ())) + "," +
+    CreateJosnArrayLine("temperature", tempValue1 + "," + tempValue2 + "," + tempValue3) + "," +
     CreateJsonLine("battery", "1024") + "," +
-    CreateJsonLine("light", "[" + lightValue1 + "," + lightValue2 + "," + lightValue3 + "]") +
+    CreateJosnArrayLine("light", lightValue1 + "," + lightValue2 + "," + lightValue3) +
   "}";
 }
 
 String CreateJsonLine(String key, String value){
   return "\"" + key + "\":" + "\"" + value + "\"";
 }
+
+String CreateJosnArrayLine(String key, String arr){
+   return "\"" + key + "\":" + "[" + arr + "]";
+}
+
 
