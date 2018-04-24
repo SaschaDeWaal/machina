93e502eeb46a7ce4cbed8e99b301ed6ef047524f
beatnory <info@dreamfoxgames.com>
2018-04-24 15:49:33 +0200

    Calibrate light

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -15,6 +15,10 @@ const int updateTempEvery = 20;
 int index = 0;
 int sensorUpdate = 0;
 
+int lightVoltage1 = 0;
+int lightVoltage2 = 0;
+int lightVoltage3 = 0;
+
 MPU6050 mpu6050(Wire);
 
 OneWire tempSensorWire1(pinTempSensor1);
@@ -31,6 +35,8 @@ void setup() {
   mpu6050.calcGyroOffsets();
   tempSensor1.begin();
   tempSensor2.begin();
+
+  CalibrateLight();
 }
 
 void loop() {
@@ -55,9 +61,9 @@ void loop() {
 }
 
 String CreateJson(int messageIndex){
-  String lightValue1 = String(analogRead(pinLight1));
-  String lightValue2 = String(analogRead(pinLight2));
-  String lightValue3 = String(analogRead(pinLight3));
+  String lightValue1 = (analogRead(pinLight1) < lightVoltage1) ? "false" : "true";//String(analogRead(pinLight1));
+  String lightValue2 = (analogRead(pinLight2) < lightVoltage2) ? "false" : "true";//String(analogRead(pinLight2));
+  String lightValue3 = (analogRead(pinLight3) < lightVoltage3) ? "false" : "true";//String(analogRead(pinLight3));
 
   String tempValue1 = String(tempSensor1.getTempCByIndex(0));
   String tempValue2 = String(tempSensor2.getTempCByIndex(0));
@@ -66,7 +72,7 @@ String CreateJson(int messageIndex){
   return "{" +
     CreateJsonLine("index", String(messageIndex)) + "," +
     CreateJosnArrayLine("gyroscoop", String(mpu6050.getGyroAngleX()) + "," + String(mpu6050.getGyroAngleY()) + "," + String(mpu6050.getGyroAngleZ())) + "," +
-    CreateJosnArrayLine("CreateJosnArrayLine", String(mpu6050.getAccX()) + "," + String(mpu6050.getAccY()) + "," + String(mpu6050.getAccZ())) + "," +
+    CreateJosnArrayLine("accelerator", String(mpu6050.getAccX()) + "," + String(mpu6050.getAccY()) + "," + String(mpu6050.getAccZ())) + "," +
     CreateJosnArrayLine("temperature", tempValue1 + "," + tempValue2 + "," + tempValue3) + "," +
     CreateJsonLine("battery", "1024") + "," +
     CreateJosnArrayLine("light", lightValue1 + "," + lightValue2 + "," + lightValue3) +
@@ -74,11 +80,32 @@ String CreateJson(int messageIndex){
 }
 
 String CreateJsonLine(String key, String value){
-  return "\"" + key + "\":" + "\"" + value + "\"";
+  return "\"" + key + "\":" + value;
 }
 
 String CreateJosnArrayLine(String key, String arr){
    return "\"" + key + "\":" + "[" + arr + "]";
+}
+
+void CalibrateLight() {
+  delay(100);
+
+  int precision = 10;
+
+  lightVoltage1 = analogRead(pinLight1);
+  lightVoltage2 = analogRead(pinLight2);
+  lightVoltage3 = analogRead(pinLight3);
+
+  for(int i = 1; i < precision; i++){
+    lightVoltage1 += analogRead(pinLight1);
+    lightVoltage2 += analogRead(pinLight2);
+    lightVoltage3 += analogRead(pinLight3);
+    delay(100);
+  }
+
+  lightVoltage1 = (lightVoltage1 / precision) * 0.8;
+  lightVoltage2 = (lightVoltage2 / precision) * 0.8;
+  lightVoltage3 = (lightVoltage3 / precision) * 0.8;
 }
 
 
