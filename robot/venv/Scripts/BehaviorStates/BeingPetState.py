e51167b3bc901e854de321e61f42f84c79488b48
Ward Bannink <w.r.j.bannink@students.uu.nl>
2018-06-12 14:30:27 +0200

    Added code for emotion; specifically the being pet state. It now checks for light values and based on that whether it's being pet (higher than 200 in all three channels)

--- robot/venv/Scripts/BehaviorStates/BeingPetState.py
+++ robot/venv/Scripts/BehaviorStates/BeingPetState.py
@@ -11,15 +11,20 @@ class BeingPetState(BaseState):
     def onEnter(self):
         super(BeingPetState, self).onEnter()
         self.timer = 5
+        self.beingPet = False
 
     def onLeave(self):
         super(BeingPetState, self).onLeave()
 
     def onUpdate(self, delta):
         super(BeingPetState, self).onUpdate(delta)
+        if self.robotData.light[0] > 200 and self.robotData.light[1] > 200 and self.robotData.light[2] > 200:
+            self.beingPet = True
         self.timer -= delta
         if self.timer <= 0:
             self.robotData.setArousal(self.robotData.arousal - 1)
             self.timer = 5
-        if not self.robotData.isBeingPetted:
+        if not self.beingPet:
             self.onLeave()
+        #if not self.robotData.isBeingPetted:
+        #    self.onLeave()
