d783dd140ff366072a6ee530d25545136634f0d4
Ward Bannink <w.r.j.bannink@students.uu.nl>
2018-06-18 14:51:05 +0200

    Implemented RemoveSnowState. It randomly picks left or right to search for snow in that direction. Once snow is found, the robot drives forward for 2 seconds, followed by 2 back, and restarts the process again. If the base is seen, it turns 180 degrees and randomizes whether it'll search left or right.
    Currently, it is set that the task is performed for 15 to 60 seconds, after which the robot will make a transition to the AttackBaseState

--- robot/venv/Scripts/BehaviorStates/RemoveSnowState.py
+++ robot/venv/Scripts/BehaviorStates/RemoveSnowState.py
@@ -8,21 +8,27 @@ class RemoveSnowState(DriveState):
 
     def __init__(self):
         super(RemoveSnowState, self).__init__()
-        self.timer = 2
+        self.timer = random.randrange(15, 60, 1)
+        self.leftRight = 2
 
     def onEnter(self):
         super(RemoveSnowState, self).onEnter()
+        self.timer = random.randrange(15, 60, 1)
         super(RemoveSnowState, self).SetCurFunction(2, "TimeDriveForward", 0)
-        #self.timer = 2
+        super(RemoveSnowState, self).SetCurFunction(1.5, "TimeDriveBackward", 0)
         #super(RemoveSnowState, self).TimeDriveForward()
 
     def onLeave(self):
         super(RemoveSnowState, self).onLeave()
-        BaseState.BaseState.goToState("AngeredState")
+        BaseState.BaseState.goToState(self, "AttackBaseState")
 
     def onUpdate(self, delta):
         super(RemoveSnowState, self).onUpdate(delta)
         self.timer -= delta
+        if self.timer <= 0:
+            self.onLeave()
+        else:
+            self.lookAround()
 
 
     """
@@ -37,15 +43,19 @@ class RemoveSnowState(DriveState):
                 self.leftRight = -1
         # Do pole and colour detection here
         baseFound = False
+        snowFound = False
+        if baseFound:
+            super(RemoveSnowState, self).SetCurFunction(10000, "TurnDegrees", 180)
+            super(RemoveSnowState, self).SetCurFunction(1, "TimeDriveBackward", 0)
+            if self.leftRight == -1:
+                self.leftRight = 1
+            elif self.leftRight == 1:
+                self.leftRight = -1
         if not baseFound:
-            self.DriveState.DriveState.TurnDegrees(5 * self.leftRight)
-        elif baseFound: # this should include an 'and' where it states there is white (snow) in sight
-            self.DriveState.DriveState.TurnDegrees(90 * self.leftRight)
-            self.DriveState.DriveState.timer = 1
-            self.DriveState.DriveState.TimeDriveForward()
-            self.DriveState.DriveState.TurnDegrees(135 * (-1*self.leftRight))
-        elif baseFound: # this should include an 'and' where it states there is no white (snow) in sight
-            # Drive towards base
-            self.DriveState.DriveState.TimeDriveForward(2)
-            self.leftRight = 2
-            self.onLeave()
+            if snowFound:
+                super(RemoveSnowState, self).SetCurFunction(2, "TimeDriveForward", 0)
+                super(RemoveSnowState, self).SetCurFunction(2, "TimeDriveBackward", 0)
+                self.leftRight = 2
+                #self.DriveState.DriveState.TurnDegrees(5 * self.leftRight)
+        if not baseFound and not snowFound:
+            super(RemoveSnowState, self).SetCurFunction(10000, "TurnDegrees", 5 * self.leftRight)
